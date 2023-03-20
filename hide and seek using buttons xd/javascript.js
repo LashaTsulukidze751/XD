f7d898f73a22e75091c4f73119b7d40a0b2ab664
lasha tsulukidze <121624660+LashaTsulukidze751@users.noreply.github.com>
2023-03-20 21:49:39 +0400

    Add files via upload

---
+++ hide and seek using buttons xd/javascript.js
@@ -0,0 +1,12 @@
+function lasha() {
+    document.getElementById("demo1").style.display = "none";
+}
+function lasha1() {
+    document.getElementById("demo1").style.display = "inline";
+}
+function lasha3() {
+    document.getElementById("demo2").style.display = "none";
+}
+function lasha2() {
+    document.getElementById("demo2").style.display = "inline";
+}
